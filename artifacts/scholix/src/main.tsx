0caadc8fb5fbb1a70b26cf1146fab0a9577e7de9
scholixapp-create <scholix.app@gmail.com>
2026-07-01 22:30:46 +1000

    connecting api to frontend

--- artifacts/scholix/src/main.tsx
+++ artifacts/scholix/src/main.tsx
@@ -1,7 +1,10 @@
+import { setBaseUrl } from "@workspace/api-client-react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { setAuthTokenGetter } from "@workspace/api-client-react";
+
+setBaseUrl(import.meta.env.VITE_API_URL ?? "");
 
 setAuthTokenGetter(() => localStorage.getItem("scholix_token"));
 
